1b1d6cfd38f8bcf8e03b775f6c9dd70dacd13fdf
mjt <thapa.manish16@gmail.com>
2015-08-29 20:12:34 +0000

    putting pages to new pdf file

--- pdfcutter.py
+++ pdfcutter.py
@@ -2,11 +2,31 @@
 import copy, sys
 from PyPDF2 import PdfFileWriter, PdfFileReader
 
-print(sys.stdin);
+print(sys.argv);
+arg = sys.argv #arguments passed
+condition = arg[1]
+inputFileName = arg[2];
+pagesToMerge = arg[3:-1];
+outputFile = arg[-1];
+
+# print(inputFileName,outputFile);
+print ("starting ... ");
 output = PdfFileWriter()
 input1 = PdfFileReader(open("LFS-BOOK-7.3.pdf", "rb"));
 # print ("document1.pdf has %d pages." + input1.getNumPages());
-print(input1.getNumPages());
+# print(input1.getNumPages());
 
-output.addPage(input1.getPage(0));
-output.addPage(input1.getPage(1));
+for i in pagesToMerge:
+	print("merging page no ", i)
+	output.addPage(input1.getPage(int(i)));
+
+# outputStream = file("sample.pdf", "wb")
+# output.write(outputStream)
+print("creating file",outputFile)
+with open(outputFile, "wb") as outputStream:
+    output.write(outputStream)
+
+print("enjoy your file ")
+
+# to excute
+# python pdfcutter.py --make filename 2 3 4 hello.pdf
